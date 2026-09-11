f718fe1119f13d639eb23baf0a37665931da8fe7
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-11 09:20:44 +0200

    feat: add sql queries for category

--- db/queries.js
+++ db/queries.js
@@ -5,6 +5,30 @@ async function getCategories() {
   return rows;
 }
 
+async function getCategory(categoryId) {
+  const SQL = `
+    SELECT name, description
+    FROM categories
+    WHERE categories.id = $1
+  `;
+
+  const { rows } = await pool.query(SQL, [categoryId]);
+  return rows[0];
+}
+
+async function getTeasByCategory(categoryId) {
+  const SQL = `
+    SELECT t.id, t.name, t.description, t.price, t.stock, o.country, o.region
+    FROM teas AS t JOIN origins AS o ON t.origin_id = o.id
+    WHERE t.category_id = $1
+  `;
+
+  const { rows } = await pool.query(SQL, [categoryId]);
+  return rows;
+}
+
 export const db = {
   getCategories,
+  getCategory,
+  getTeasByCategory,
 };
