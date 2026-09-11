ef1df012d515fe082ed36a909d054185d69921ad
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-11 11:12:33 +0200

    feat: add current path to res.locals

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 8080;
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+app.use((req, res, next) => {
+  res.locals.currentPath = req.path;
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
