0770503e582bbd54801395987f95278e8679290f
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-12 10:28:06 +0200

    feat: add GET and POST routes for new product form

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,6 +4,9 @@ import productsController from "../controllers/productsController.js";
 const productsRouter = Router();
 
 productsRouter.get("/", productsController.getProducts);
+productsRouter.get("/new", productsController.getNewProductForm);
 productsRouter.get("/:id", productsController.getProduct);
+
+productsRouter.post("/new", productsController.createProduct);
 
 export default productsRouter;
