03c5f89aef5f28d538b9cc143098f80220e55dce
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-12 13:10:26 +0200

    refactor: organize product routes

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,12 +4,15 @@ import productsController from "../controllers/productsController.js";
 const productsRouter = Router();
 
 productsRouter.get("/", productsController.getProducts);
+
 productsRouter.get("/new", productsController.getNewProductForm);
+productsRouter.post("/new", productsController.createProduct);
+
 productsRouter.get("/:id/edit", productsController.getEditProduct);
+productsRouter.post("/:id/edit", productsController.updateProduct);
+
 productsRouter.get("/:id", productsController.getProduct);
 
-productsRouter.post("/:id/edit", productsController.updateProduct);
 productsRouter.post("/:id/delete", productsController.deleteProduct);
-productsRouter.post("/new", productsController.createProduct);
 
 export default productsRouter;
