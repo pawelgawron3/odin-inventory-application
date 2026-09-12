d5e711bdd798b93a6860b347190d57a94e984478
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-12 11:47:38 +0200

    feat: add new product routes

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -5,8 +5,11 @@ const productsRouter = Router();
 
 productsRouter.get("/", productsController.getProducts);
 productsRouter.get("/new", productsController.getNewProductForm);
+productsRouter.get("/:id/edit", productsController.getEditProduct);
 productsRouter.get("/:id", productsController.getProduct);
 
+productsRouter.post("/:id/edit", productsController.updateProduct);
+// productsRouter.post("/:id/delete", productsController.deleteProduct);
 productsRouter.post("/new", productsController.createProduct);
 
 export default productsRouter;
