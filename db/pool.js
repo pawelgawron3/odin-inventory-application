b7553a1ebd841f76642be66cb3caf5ebd389b47c
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-14 12:51:39 +0200

    chore: update the connectionString

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,7 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
