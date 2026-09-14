56a6122337458fb78da0e5b2dd8cc23a6e161921
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-14 11:36:56 +0200

    feat: add new origin functionalities

--- controllers/originController.js
+++ controllers/originController.js
@@ -1,8 +1,15 @@
 import { db } from "../db/queries.js";
 
 const PSQL_UNIQUE_VIOLATION = "23505";
+const PSQL_FOREIGN_KEY_VIOLATION = "23503";
 
 const originController = {
+  async getOrigins(req, res) {
+    const origins = await db.getOrigins();
+
+    res.render("origins", { origins, error: null });
+  },
+
   getNewOriginForm(req, res) {
     res.render("new-origin", { error: null, origin: null });
   },
@@ -19,6 +26,28 @@ const originController = {
         return res.status(400).render("new-origin", {
           error: "This origin already exists!",
           origin,
+        });
+      }
+
+      throw err;
+    }
+  },
+
+  async deleteOrigin(req, res) {
+    const originId = req.params.id;
+
+    try {
+      await db.deleteOrigin(originId);
+
+      res.redirect("/origins");
+    } catch (err) {
+      if (err.code === PSQL_FOREIGN_KEY_VIOLATION) {
+        const origins = await db.getOrigins();
+
+        return res.status(400).render("origins", {
+          origins,
+          error:
+            "This origin cannot be deleted because it is used by existing teas.",
         });
       }
 
--- routes/originRouter.js
+++ routes/originRouter.js
@@ -3,7 +3,11 @@ import originController from "../controllers/originController.js";
 
 const originRouter = Router();
 
+originRouter.get("/", originController.getOrigins);
+
 originRouter.get("/new", originController.getNewOriginForm);
 originRouter.post("/new", originController.createOrigin);
+
+originRouter.post("/:id/delete", originController.deleteOrigin);
 
 export default originRouter;
